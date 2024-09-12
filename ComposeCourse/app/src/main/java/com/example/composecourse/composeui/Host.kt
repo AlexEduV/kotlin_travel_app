5d4f4d64283037567ed96d3e97cfeda1241003dd
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-12 15:28:47 +0300

    finished host card;

--- ComposeCourse/app/src/main/java/com/example/composecourse/composeui/Host.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/composeui/Host.kt
@@ -1,5 +1,6 @@
 package com.example.composecourse.composeui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -7,18 +8,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -59,13 +64,13 @@ fun HostCard() {
                 .horizontalScroll(photoScrollState)
         ) {
 
-            ApartmentPhotoItem()
+            ApartmentPhotoItem(R.drawable.apartment_1)
 
-            ApartmentPhotoItem()
+            ApartmentPhotoItem(R.drawable.apartment_2)
 
-            ApartmentPhotoItem()
+            ApartmentPhotoItem(R.drawable.apartment_3)
 
-            ApartmentPhotoItem()
+            ApartmentPhotoItem(R.drawable.apartment_4)
 
         }
 
@@ -101,11 +106,22 @@ fun HostInfoColumn(
 }
 
 @Composable
-fun ApartmentPhotoItem() {
+fun ApartmentPhotoItem(resourceId: Int) {
 
     Box(modifier = Modifier
         .height(60.dp)
         .width(100.dp)
-        .background(Color.Gray.copy(alpha = 0.9f), RoundedCornerShape(4.dp))
-    )
+        .background(
+            Color.Gray.copy(alpha = 0.9f),
+            RoundedCornerShape(4.dp))
+    ) {
+        Image(
+            painter = painterResource(id = resourceId),
+            contentDescription = "Apartment Image",
+            modifier = Modifier
+                .clip(RoundedCornerShape(4.dp))
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop,
+        )
+    }
 }
