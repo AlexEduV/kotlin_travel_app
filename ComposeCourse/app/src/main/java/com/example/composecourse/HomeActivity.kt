dc1c4d4010e867beb06eba9ff5e48bf096af81b9
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-12 15:47:06 +0300

    working on the search section;

--- ComposeCourse/app/src/main/java/com/example/composecourse/HomeActivity.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/HomeActivity.kt
@@ -7,13 +7,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composecourse.composeui.BackButton
+import com.example.composecourse.composeui.CardSubtitle
 import com.example.composecourse.composeui.HostCard
 
 class HomeActivity : ComponentActivity() {
@@ -43,6 +55,48 @@ class HomeActivity : ComponentActivity() {
                     BackButton()
 
                     HostCard()
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
+
+                        Text(
+                            "Searching Results".uppercase(),
+                            color = Color.White,
+                            fontSize = 16.sp,
+                        )
+
+                        Text(
+                            "Hotels & Apartments",
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.W600,
+                        )
+
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(4.dp)
+                            ) {
+
+                            Icon(
+                                imageVector = Icons.Filled.LocationOn,
+                                contentDescription = "location marker",
+                                tint = Color.White,
+                                modifier = Modifier.size(15.dp)
+                            )
+
+                            Text(
+                                "Mostar, Bosnia",
+                                color = Color.White,
+                                fontSize = 15.sp,
+                            )
+                        }
+
+                        //search selector here
+                        
+
+
+                    }
 
                 }
             }
