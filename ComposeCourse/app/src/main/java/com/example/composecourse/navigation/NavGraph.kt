1c8d2c7d93085858bfcee96f174893c1a78fc236
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-12 21:16:53 +0300

    now passing id as a route parameter;

--- ComposeCourse/app/src/main/java/com/example/composecourse/navigation/NavGraph.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/navigation/NavGraph.kt
@@ -1,9 +1,12 @@
 package com.example.composecourse.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.composecourse.screens.ApartmentDetailsScreen
 import com.example.composecourse.screens.HomeScreen
 import com.example.composecourse.screens.ProfileDetailsScreen
@@ -17,6 +20,12 @@ fun NavGraph(navController: NavHostController) {
     ) {
         composable(Destinations.Home.route) { HomeScreen(navController) }
         composable(Destinations.ProfileDetails.route) { ProfileDetailsScreen(navController) }
-        composable(Destinations.ApartmentDetails.route) { ApartmentDetailsScreen(navController) }
+        composable(
+            route = "${Destinations.ApartmentDetails.route}/{itemId}",
+            arguments = listOf(navArgument("itemId") { type = NavType.IntType})
+        ) { backStackEntry ->
+
+            val itemId = backStackEntry.arguments?.getInt("itemId")
+            ApartmentDetailsScreen(navController, itemId = itemId) }
     }
 }
--- ComposeCourse/app/src/main/java/com/example/composecourse/screens/ApartmentDetailsScreen.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/screens/ApartmentDetailsScreen.kt
@@ -1,10 +1,26 @@
 package com.example.composecourse.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import com.example.composecourse.compose.BackButton
 
 
 @Composable
-fun ApartmentDetailsScreen(navHostController: NavHostController) {
+fun ApartmentDetailsScreen(
+    navHostController: NavHostController,
+    itemId: Int?
+    ) {
+
+    Column {
+
+        Row {
+
+            BackButton()
+
+        }
+
+    }
 
 }
