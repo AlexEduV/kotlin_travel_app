66acc07d57716ec86d5931bad33757fd56150858
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-12 12:12:29 +0300

    upgraded main card shadow; fixed background button color;

--- ComposeCourse/app/src/main/java/com/example/composecourse/composeui/CustomCard.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/composeui/CustomCard.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.sharp.Share
@@ -25,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
@@ -43,7 +45,12 @@ fun CustomCard(
     Card(
         colors = CardDefaults.cardColors()
             .copy(containerColor = backgroundColor),
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = Modifier
+            .graphicsLayer(
+                shadowElevation = 16.0f,
+                shape = RoundedCornerShape(16.dp),
+                clip = true,
+                )
     )
     {
 
@@ -102,7 +109,7 @@ fun CustomCard(
                     onClick = {},
                     shape = CircleShape,
                     border = BorderStroke(width = 1.dp, color = buttonColor),
-                    colors = ButtonDefaults.buttonColors().copy(containerColor = Color.White, contentColor = buttonColor),
+                    colors = ButtonDefaults.buttonColors().copy(containerColor = backgroundColor, contentColor = buttonColor),
                     modifier = Modifier
                         .fillMaxHeight()
                         .aspectRatio(1f),
