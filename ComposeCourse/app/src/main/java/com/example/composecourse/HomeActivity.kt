fba61ced2b281628a8d5d915116965de0a911bbc
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-12 13:45:29 +0300

    added a second empty activity;

---
+++ ComposeCourse/app/src/main/java/com/example/composecourse/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.composecourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
