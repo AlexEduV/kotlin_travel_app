944b8402aff7cfb7f1442c8534de1bf12aa5bcfc
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-13 11:27:49 +0300

    minor changes;

--- ComposeCourse/app/src/main/java/com/example/composecourse/screens/ApartmentDetailsScreen.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/screens/ApartmentDetailsScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.DateRange
@@ -163,13 +161,7 @@ fun ApartmentDetailsScreen(
             )
         }
 
-        //'Book Now' button
-
-
-        
-        
-        
-        
+        //todo: 'Book Now' button
 
     }
 
