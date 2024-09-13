2387513705c35f99db1e113fe8389cf22d475ee7
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-13 09:15:14 +0300

    fixed main image in details;

--- ComposeCourse/app/src/main/java/com/example/composecourse/screens/ApartmentDetailsScreen.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/screens/ApartmentDetailsScreen.kt
@@ -1,13 +1,9 @@
 package com.example.composecourse.screens
 
-import android.widget.Space
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -15,20 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.DateRange
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
@@ -74,16 +63,17 @@ fun ApartmentDetailsScreen(
 
         Box(modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(0.7f)
-            .clip(SemicircleShape())
+            .fillMaxHeight(0.6f)
+            .padding(horizontal = 16.dp)
         ) {
 
             Image(
                 painter = painterResource(id = itemId as Int),
                 contentDescription = "Large Scale Apartment Picture",
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize(),
-
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(SemiCircleShape())
             )
 
         }
@@ -96,26 +86,27 @@ fun ApartmentDetailsScreen(
 
 }
 
-class SemicircleShape : Shape {
+class SemiCircleShape : Shape {
     override fun createOutline(
         size: Size,
         layoutDirection: LayoutDirection,
         density: Density
     ): Outline {
-        return Outline.Generic(
-            Path().apply {
-                moveTo(0f, size.height/ 2)
-                arcTo(
-                    rect = Rect(0f, -size.height / 2, size.width, size.height / 2),
-                    startAngleDegrees = 180f,
-                    sweepAngleDegrees = 180f,
-                    forceMoveTo = false
-                )
-                lineTo(size.width, size.height)
-                lineTo(0f, size.height)
-                close()
-            }
-        )
+
+        val path = Path().apply {
+            moveTo(0f, size.height / 2)
+            arcTo(
+                rect = Rect(0f, 0f, size.width, size.height),
+                startAngleDegrees = 180f,
+                sweepAngleDegrees = 180f,
+                forceMoveTo = false
+            )
+            lineTo(size.width, size.height)
+            lineTo(0f, size.height)
+            close()
+        }
+
+        return Outline.Generic(path)
     }
 
 }
