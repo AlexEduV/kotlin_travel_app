408cb12eb241a0590b2c66d8221b83316bbb69c9
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-13 09:49:16 +0300

    updated custom shape clas to include a parameter for divider factor;

--- ComposeCourse/app/src/main/java/com/example/composecourse/compose/Shapes.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/compose/Shapes.kt
@@ -8,17 +8,18 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 
-class SemiCircleShape : Shape {
+class SemiCircleShape(private val arcHeightFactor: Float = 3f) : Shape {
     override fun createOutline(
         size: Size,
         layoutDirection: LayoutDirection,
         density: Density
     ): Outline {
 
+        val arcHeight = size.height / arcHeightFactor
         val path = Path().apply {
-            moveTo(0f, size.height / 3)
+            moveTo(0f, arcHeight)
             arcTo(
-                rect = Rect(0f, 0f, size.width, size.height / 1.5f),
+                rect = Rect(0f, 0f, size.width, arcHeight * 2),
                 startAngleDegrees = 180f,
                 sweepAngleDegrees = 180f,
                 forceMoveTo = false
