254608c83ca719d6a280c012583c0580e5c7b211
Alexander Aks'onov <m.mirvua@gmail.com>
2024-09-13 21:37:48 +0300

    added padding to the host image to prevent aliasing;

--- ComposeCourse/app/src/main/java/com/example/composecourse/screens/ApartmentDetailsScreen.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/screens/ApartmentDetailsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.composecourse.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -23,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -97,7 +99,8 @@ fun ApartmentDetailsScreen(
                     .size(100.dp)
                     .offset(y = 40.dp)
                     .align(Alignment.BottomCenter)
-                    .border(width = 8.dp, MaterialTheme.colorScheme.background, CircleShape)
+                    .border(width = 8.dp, Color.White, CircleShape)
+                    .padding(5.dp)
                     .clip(CircleShape)
             )
 
